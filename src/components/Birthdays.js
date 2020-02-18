43c7c5fe5b2c9c33a62509a2c045b2c847929bf7
Jan <jantheprogrammer@gmail.com>
2020-02-17 21:41:53 -0800

    DatePickers added

--- src/components/Birthdays.js
+++ src/components/Birthdays.js
@@ -1,22 +1,37 @@
 import React, {useState, useEffect} from 'react'
-import {Row} from 'antd'
+import {Row, DatePicker} from 'antd'
 import axios from 'axios'
+import moment from 'moment'
+
 import {eventsApi} from '../utils/api'
 import MyLayout from './MyLayout'
 
 function Birthdays() {
-  // const [birthdays, setBirthdays] = useState([])
+  const [birthdays, setBirthdays] = useState([])
+  const [toDate, setToDate] = useState(['2021-01-01'])
 
-  // useEffect(() => {
-  //   axios
-  //     .get(eventsApi.get)
-  //     .then(res => {
-  //       setBirthdays(res.data.filter(e => e.summary === 'Birthday'))
-  //     })
-  //     .catch(err => {
-  //       console.error(err)
-  //     })
-  // }, [])
+  const dateSuffix = 'T00:00:00.0Z'
+
+  useEffect(() => {
+    axios
+      .get(eventsApi.get, {
+        params: {
+          to_date: toDate + dateSuffix,
+        },
+      })
+      .then(res => {
+        console.log(res.data)
+        setBirthdays(res.data.filter(e => e.summary === 'Birthday'))
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [toDate])
+
+  console.log('b: ', birthdays)
+
+  // List of birthdays in whole year and print it in nice way
+  // Finish summary of reading in overview in any form
 
   return (
     <MyLayout>
@@ -24,7 +39,24 @@ function Birthdays() {
         <div className='heading'>
           <h2>Birthdays</h2>
         </div>
-        <div className='content'>Bday</div>
+        <div className='content'>
+          <DatePicker
+            // disabledDate={this.disabledStartDate}
+            format='YYYY-MM-DD'
+            // value={startValue}
+            placeholder='Start'
+            // onOpenChange={this.handleStartOpenChange}
+          />
+          <DatePicker
+            // disabledDate={this.disabledEndDate}
+            format='YYYY-MM-DD'
+            defaultValue={moment(toDate)}
+            placeholder='End'
+            onChange={(event, value) => setToDate(value)}
+            // open={endOpen}
+            // onOpenChange={this.handleEndOpenChange}
+          />
+        </div>
       </Row>
     </MyLayout>
   )
